93617e773773d1e97daf63b7b9535595c6b628ac
byui-dev <okdiphala@hotmail.com>
2026-05-08 15:00:16 +0200

    Add new files

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Obakeng Diphala!');
-});
+const port = 3000;
 
-const port = 3001;
-console.log('Web server is listening at port' * (process.env.port || port));
+app.use('/', require('./routes/index'));
+
+app.listen(process.env.port || port);
+console.log('Web server is listening at port ' * (process.env.port || port));
